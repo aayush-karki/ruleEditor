df71874823b39dc9bb9c695e7888c9affd8cc051
Aayush Karki <akarki@ramapo.edu>
2023-11-29 19:42:52 -0500

    cleans up the exportBtnOnClick and convertToJson functions

--- src/public/ruleWriter.js
+++ src/public/ruleWriter.js
@@ -8,36 +8,25 @@
 		const ruleForm = document
 			.getElementsByClassName("rules_container")
 			.item(0);
-		const rulesCollection = ruleForm.childNodes;
-		const rulesCollection1 = ruleForm.childNodes[0];
-		// console.log(ruleForm);
-		console.log(rulesCollection);
-		console.log("reutrnObject: ");
-		// console.log(rulesCollection1);
-		const returnObj = convertToJson(rulesCollection);
+		const ruleConditions = ruleForm.childNodes[0];
+		const returnObj = convertToJson(ruleConditions);
 
-		console.log("reutrnObject: ", returnObj);
+		console.log("reutrnObject: ", JSON.stringify(returnObj));
 	};
 
-	function convertToJson(ruleCollection, currTab = "") {
-		console.log(currTab + "\t", "ruleCollection", ruleCollection);
-		// base case 1: ruleCollection is empty
-		if (ruleCollection.length === 0) {
-			return;
+	function convertToJson(a_ruleConditions) {
+		// base case 1: a_ruleConditions is empty
+		if (a_ruleConditions === undefined) {
+			return undefined;
 		}
-		const rulesConditions = ruleCollection[0];
-		console.log(currTab + "\t", "rulesConditions", rulesConditions);
-		const ruleConditionitems = rulesConditions.childNodes;
-		// see the second element as that is the operator
-		// const [firstNode, operator, ...rest] = [...ruleConditionitems];
 
-		console.log(currTab + "\t", "ruleConditionitems", ruleConditionitems);
+		const ruleConditionitems = a_ruleConditions.childNodes;
+
 		// base case 2: ruleCondition has the rule
 		if (
 			ruleConditionitems.length === 3 &&
-			ruleConditionitems[0].nodeName == "INPUT"
+			ruleConditionitems[0].classList.contains("field_data")
 		) {
-			console.log(currTab + "\tbase 1");
 			return {
 				operator: ruleConditionitems[1].value,
 				field: ruleConditionitems[0].value,
@@ -45,27 +34,15 @@
 			};
 		}
 
-		// its rule conditions
 		const returnObj = {
 			operator: ruleConditionitems[1].value,
 			conditions: [],
 		};
 
 		for (let i = 0; i < ruleConditionitems.length; i += 2) {
-			let condition = convertToJson(
-				[ruleConditionitems[i]],
-				currTab + "\t"
-			);
+			let condition = convertToJson(ruleConditionitems[i]);
 			returnObj.conditions.push(condition);
 		}
-
-		// console.log(firstNode.nodeName);
-		// const firstNode = [ruleConditionitems[0]];
-		// console.log(currTab + "\t", "firstNode", firstNode);
-		// console.log(currTab + "\t", "operator", operator);
-		// console.log(currTab + "\t", "rest", rest);
-		// convertToJson([firstNode], currTab + "\t");
-		// const op = ruleCollection.
 
 		return returnObj;
 	}
