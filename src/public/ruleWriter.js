a08bc201f385873fe52c38970ad11b1a05d23b87
Aayush Karki <akarki@ramapo.edu>
2023-12-06 22:33:24 -0500

    exports rules and start working on loading

--- src/public/ruleWriter.js
+++ src/public/ruleWriter.js
@@ -1,18 +1,5 @@
 (function () {
-	"use strict";
-
-	// export button to export the data from a json
-	const exportBtn = document.getElementById("exportBtn");
-
-	const exportBtnOnClick = () => {
-		const ruleForm = document
-			.getElementsByClassName("rules_container")
-			.item(0);
-		const ruleConditions = ruleForm.childNodes[0];
-		const returnObj = convertToJson(ruleConditions);
-
-		console.log("reutrnObject: ", JSON.stringify(returnObj));
-	};
+	("use strict");
 
 	function convertToJson(a_ruleConditions) {
 		// base case 1: a_ruleConditions is empty
@@ -46,6 +33,65 @@
 
 		return returnObj;
 	}
+
+	const exportBtnOnClick = () => {
+		/**
+		 * TODO: when export is pressed a pop up will come up
+		 * 		where user can type in a file name
+		 * 		and they can press download to d
+		 */
+
+		const ruleForm = document
+			.getElementsByClassName("rules_container")
+			.item(0);
+		const ruleConditions = ruleForm.childNodes[0];
+		const rulesJson = JSON.stringify(convertToJson(ruleConditions));
+
+		// using blob to download the data as a file
+		let rulesJsonToBLOB = new Blob([rulesJson], {
+			type: "application/json",
+		});
+		const sFileName = "ruleData.txt";
+		let newLink = document.createElement("a");
+		newLink.download = sFileName;
+
+		if (window.webkitURL != null) {
+			newLink.href = window.webkitURL.createObjectURL(rulesJsonToBLOB);
+		} else {
+			newLink.href = window.URL.createObjectURL(rulesJsonToBLOB);
+			newLink.style.display = "none";
+			document.body.appendChild(newLink);
+		}
+
+		newLink.click();
+	};
+
+	// export button to export the data from a json
+	const exportBtn = document.getElementById("exportBtn");
+	const loadBtn = document.getElementById("loadBtn");
+
+	// file input
+	const loadfileInput = document.getElementById("loadfileInput");
+
+	// load the rules
+	loadfileInput.addEventListener("change", (event) => {
+		const file = event.target.files[0];
+		if (file) {
+			let reader = new FileReader();
+			reader.readAsText(file, "UTF-8");
+			reader.onload = function (evt) {
+				console.log(JSON.parse(evt.target.result));
+			};
+			reader.onerror = function (evt) {
+				console.log("error reading file");
+			};
+		}
+	});
+
+	// open the file browser
+	loadBtn.addEventListener("click", () => {
+		loadfileInput.click();
+	});
 
 	exportBtn.addEventListener("click", exportBtnOnClick);
 })();
